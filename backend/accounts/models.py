039669c0bc07535ff070dca77fe31a3f9f7c65be
andreluiisdantas <andreluissousaa10@gmail.com>
2025-10-23 16:55:45 -0300

    Criando model do User

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id_user = models.IntegerField(primary_key=True),
+    email = models.EmailField(max_length=254, unique=True),
+    nome = models.CharField(max_length=255),
+    password = models.CharField(max_length=255),
+    is_active = models.BooleanField(),
+    is_staff = models.BooleanField()
