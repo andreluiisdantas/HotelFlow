e556e550ece17ab2167b63c6c873263a53f8b7f3
andreluiisdantas <andreluissousaa10@gmail.com>
2026-01-20 18:21:36 -0300

    Status folio Repository update

--- backend/HotelFlow/src/main/java/br/com/HotelFlow/repository/StatusFolioRepository.java
+++ backend/HotelFlow/src/main/java/br/com/HotelFlow/repository/StatusFolioRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StatusFolioRepository extends JpaRepository<StatusFolioModel, Long> {
+    boolean existsByStatus(String status);
 }
