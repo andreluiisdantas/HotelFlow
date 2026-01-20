9c9b71cd9f25f886718963d6fa1c53554cebece4
andreluiisdantas <andreluissousaa10@gmail.com>
2026-01-20 18:20:52 -0300

    Room Repository update

--- backend/HotelFlow/src/main/java/br/com/HotelFlow/repository/RoomRepository.java
+++ backend/HotelFlow/src/main/java/br/com/HotelFlow/repository/RoomRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<RoomModel, Long> {
+    boolean existsByDoorCode(String doorCode);
 }
