4c20819fdb3e0776fded396ddd9a16f293e2b7fd
andreluiisdantas <andreluissousaa10@gmail.com>
2026-01-20 17:09:34 -0300

    Roles repository

--- backend/HotelFlow/src/main/java/br/com/HotelFlow/repository/RolesRepository.java
+++ backend/HotelFlow/src/main/java/br/com/HotelFlow/repository/RolesRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RolesRepository extends JpaRepository<RolesModel, Long> {
+    boolean existsByName(String name);
 }
